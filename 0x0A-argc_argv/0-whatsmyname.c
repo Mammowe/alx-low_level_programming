c9a037be0afa96cacc4d6292916dd4c6b0326aff
Mammowe <kgoputsolerato@yahoo.com>
2023-08-04 04:36:42 -0700

    Name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * Description: Function
+ * @argc: Input variable
+ * @argv: Input variable 2
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+	return (0);
+}
+
